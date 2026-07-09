8100995a24a70b8eff0fb77be339bb4d921210f5
ianhenry0431-oss <ianhenry0431@gmail.com>
2026-07-08 20:53:21 -0700

    Add Colors object with theme color definitions

---
+++ src/theme/colors.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+  background: "#121212",
+  card: "#1D1D1D",
+
+  purple: "#6C3EFF",
+
+  purpleLight: "#8E66FF",
+
+  gold: "#F4C542",
+
+  white: "#FFFFFF",
+
+  text: "#EAEAEA",
+
+  gray: "#A0A0A0",
+
+  success: "#38D996",
+
+  danger: "#F15A5A"
+};
