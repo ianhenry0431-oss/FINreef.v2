2b74b793d25dfe0df8b8a572fbcd847c87f7a390
ianhenry0431-oss <ianhenry0431@gmail.com>
2026-07-08 20:55:32 -0700

    Add GoalsScreen component for displaying goals

---
+++ GoalsScreen.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+
+export default function GoalsScreen() {
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#121212"
+      }}
+    >
+      <Text style={{color:"white",fontSize:28}}>
+        Goals
+      </Text>
+    </View>
+  );
+}
